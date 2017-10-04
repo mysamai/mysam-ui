fbc3a055accf286f20d5d4b2e5c680c43023b4ba
David Luecke <daff@neyeon.com>
2017-10-04 13:44:43 -0700

    Updating dist

--- dist/mysam-ui.js
+++ dist/mysam-ui.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["mysam"] = factory();
+		exports["mysamUi"] = factory();
 	else
-		root["mysam"] = factory();
+		root["mysamUi"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache

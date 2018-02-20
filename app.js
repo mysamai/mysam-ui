92889561d45f898f79e4ba46d16dc051e54aeae0
David Luecke <daff@neyeon.com>
2018-02-19 17:00:56 -0800

    Move to using mysam-core

--- app.js
+++
@@ -1,9 +0,0 @@
-/* global localStorage, mysam */
-
-// Initialize API (in the browser)
-const core = mysam();
-
-// Load the UI
-mysam.ui(document.getElementById('content'), core);
-
-localStorage.debug = 'mysam*';
